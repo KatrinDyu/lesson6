78a030fd72a5052bbe3f6c41bec69e6dcd4b17c1
Ekaterina <dyuldina32@gmail.com>
2023-01-27 20:14:39 +0500

    1st ex

---
+++ ex1/Program.cs
@@ -0,0 +1,25 @@
+﻿Console.Clear();
+Console.WriteLine("vvedsite chisla cherez probel ");
+int [] numbs = UserArray(Console.ReadLine());
+int [] UserArray(string stringArray)
+{
+    string[] numbers = stringArray.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+    int [] result = new int[numbers.Length];
+    for (int i = 0; i< result.Length; i++)
+    {
+        result[i] = int.Parse(numbers[i]);
+    }
+    return result;
+}
+int [] CountPositiv(int[] array)
+{
+int count = 0;
+
+        for(int i = 0; i < array.Length; i++)
+    {
+        if (array[i] > 0) count++;
+    
+    }
+    return count; //не понимаю что не так с count????
+}
+Console.WriteLine($"polojitelnih chisel -> {CountPositiv(numbs)}");
